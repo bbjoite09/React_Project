116b54aa4f7994bfb2ccc62b6cf02e57daf25805
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-13 23:48:03 +0900

    feat : 함수형 컴포넌트 state 관리

--- square/src/App.js
+++ square/src/App.js
@@ -1,57 +1,94 @@
 import React from 'react';
 
+/* class형 컴포넌트-----------------------------------------------------------
 class App extends React.Component {
 
-  constructor(props){
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      count: 3,
-    }
-  }
-
-  addSquare = () => {
-    this.setState({count: this.state.count + 1});
-    console.log('add')
-  }
-
-  removeSquare = () => {
-    if(this.state.count > 0){
-      this.setState({count: this.state.constructor - 1});
-    }
-    else{
-      window.alert('더 이상 뺄 수 없어요!')
+        this.state = {
+            count: 3,
+        }
     }
 
-    console.log('remove')
-  }
+    addSquare = () => {
+        this.setState({count: this.state.count + 1});
+        console.log('add')
+    }
 
-  render(){
-    const square_count = Array.from({length: this.state.count}, (v, i) => (i));
-    console.log(square_count);
-    return (
-      <div className="App">
-        {square_count.map((num, idx) => {
-          return (
-            <div key={idx}
-              style={{
-                width: "150px",
-                height: "150px",
-                backgroundColor: "#ddd",
-                margin: "10px",
-              }}
-            >
-          square
-        </div>
-          );
-        })}
-        <button onClick={this.addSquare}>하나 추가</button>
-        <button onClick={this.removeSquare}>하나 빼기</button>
+    removeSquare = () => {
+        if (this.state.count > 0) {
+            this.setState({count: this.state.count - 1});
+        } else {
+            window.alert('더 이상 뺄 수 없어요!')
+        }
 
-      </div>
-    );
-  }
+        console.log('remove')
+    }
+
+    render() {
+        const square_count = Array.from({length: this.state.count}, (v, i) => (i));
+        console.log(square_count);
+        return (
+            <div className="App">
+                {square_count.map((num, idx) => {
+                    return (
+                        <div key={idx}
+                             style={{
+                                 width: "150px",
+                                 height: "150px",
+                                 backgroundColor: "#ddd",
+                                 margin: "10px",
+                             }}
+                        >
+                            square
+                        </div>
+                    );
+                })}
+                <button onClick={this.addSquare}>하나 추가</button>
+                <button onClick={this.removeSquare}>하나 빼기</button>
+
+            </div>
+        );
+    }
 }
+------------------------------------------------------------------------ */
+import Square from "./Square";
 
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 3,
+        }
+    }
+
+    addSquare = () => {
+        this.setState({count: this.state.count + 1});
+        console.log('add')
+    }
+
+    removeSquare = () => {
+        if (this.state.count > 0) {
+            this.setState({count: this.state.count - 1});
+        } else {
+            window.alert('더 이상 뺄 수 없어요!')
+        }
+
+        console.log('remove')
+    }
+
+    render() {
+        const square_count = Array.from({length: this.state.count}, (v, i) => (i));
+        console.log(square_count);
+        return (
+            <div className="App">
+                <Square/>
+            </div>
+        );
+    }
+}
 
 export default App;
