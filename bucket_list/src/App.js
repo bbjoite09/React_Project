2b54afd9285d0466a98c1afb2dc8f5c36f85d49d
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-15 23:29:23 +0900

    style : UPDATE bucket_list/App.js code style change

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -4,41 +4,37 @@ import logo from "./logo.svg";
 // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
 import BucketList from "./BucketList";
 import styled from "styled-components";
+import scss_ex from "./scss_ex.scss"
 
 // 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    // App 컴포넌트의 state를 정의해줍니다.
-    this.state = {
-      list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
-    };
+    constructor(props) {
+        super(props);
+        // App 컴포넌트의 state를 정의해줍니다.
+        this.state = {
+            list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
+        };
+        this.text = React.createRef();
+    }
 
-    this.text = React.createRef();
-  }
-  componentDidMount() {
-    console.log(this.text);
-    console.log(this.text.current);
-  }
-
-
-  // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
-  render() {
-    return (
-      <div className="App">
-        <Container>
-          <Title >내 버킷리스트</Title>
-          <Line/>
-          {/* 컴포넌트를 넣어줍니다. */}
-          {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
-          <BucketList list={this.state.list} />
-        </Container>
-        <div>
-          <input type="text" ref={this.text} />
-        </div>
-      </div>
-    );
-  }
+    // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
+    render() {
+        return (
+            <div className="App">
+                <Container>
+                    <Title>내 버킷리스트</Title>
+                    <Line/>
+                    {/* 컴포넌트를 넣어줍니다. */}
+                    {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
+                    <BucketList list={this.state.list}/>
+                </Container>
+                <Container>
+                    <input type="text" ref={this.text}/>
+                    <button>추가하기</button>
+                </Container>
+            </div>
+        );
+    }
 }
 
 const Container = styled.div`
