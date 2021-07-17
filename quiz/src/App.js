20cb349d246dbfd7ebe6f8d70fdd4f811f031f4f
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-18 01:12:30 +0900

    feat : Swipe Quiz Project(나중에 다시)

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Score from "./Score";
+import React from "react";
+import Quiz from "./Quiz";
+import Start from "./Start";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "Heee-on",
+            page: "quiz",
+            scoreMsg: "이 정도면 아주 친한 친구 사이! 앞으로도 더 친하게 지내요 :)",
+            list: [
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+                {question: "르탄이는 2살이다.", answer: "O"},
+            ],
+            ranking: [
+                {rank: 1, name: "홍길동", message: "안녕 르탄아!"},
+                {rank: 1, name: "홍길동", message: "안녕 르탄아!"},
+                {rank: 1, name: "홍길동", message: "안녕 르탄아!"},
+                {rank: 1, name: "홍길동", message: "안녕 르탄아!"},
+                {rank: 1, name: "홍길동", message: "안녕 르탄아!"},
+                {rank: 1, name: "홍길동", message: "안녕 르탄아!"},
+                {rank: 1, name: "홍길동", message: "안녕 르탄아!"},
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {/* 조건부 랜더링을 합니다 / state의 page를 바꿔가면서 확인해봐요! */}
+                {this.state.page === "quiz" && (<Quiz list={this.state.list}/>)}
+                {this.state.page === "start" && (<Start name={this.state.name}/>)}
+                {this.state.page === "score" && (<Score name={this.state.name} scoreMsg={this.state.scoreMsg}/>)}
+            </div>
+        );
+    }
 }
 
 export default App;
