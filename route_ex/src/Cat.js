8ab1325b4422a4ee953db1175afc2ef5264898a7
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-23 23:21:25 +0900

    feat : history

--- route_ex/src/Cat.js
+++ route_ex/src/Cat.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Cat = (props) => {
-    return(<div>고양이</div>)
+    console.log(props.match);
+    return(<div>내 고양이 이름은 {props.match.params.cat_name}에요!</div>)
 }
 
 export default Cat;
