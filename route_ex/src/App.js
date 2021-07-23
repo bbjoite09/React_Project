ac68751b3da8a4a6fae70b905cc1f5e4a27cac03
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-23 22:13:31 +0900

    feat : 라우팅 처리

--- route_ex/src/App.js
+++ route_ex/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { render } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route } from "react-router-dom";
+
+import Home from "./Home";
+import Cat from "./Cat";
+import Dog from "./Dog";
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Route path="/" component={Home}/>
+                <Route path="/cat" component={Cat}/>
+                <Route path="/dog" component={Dog}/>
+            </div>
+        );
+    }
 }
 
 export default App;
