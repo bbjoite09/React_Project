72ba6083ae3750e9b4eaa8098408b1274f94bd57
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-07-25 02:22:06 +0900

    feat : ActionCreators, Reducer

--- bucket_list/src/redux/modules/bucket.js
+++ bucket_list/src/redux/modules/bucket.js
@@ -4,20 +4,39 @@
 const LOAD = "bucket/LOAD";
 const CREATE = "bucket/CREATE";
 
+// initialState
 const initialState = {
     list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
 };
 
+//Action Creators - export default 는 파일당 1개만 가능하므로 그냥 export
+export const loadBucket = (bucket) => {
+    return {type: LOAD, bucket};
+}
+
+export const createBucket = (bucket) => {
+    return {type: CREATE, bucket}
+}
 
 // Reducer
-export default function reducer(state = {}, action = {}) {
-  switch (action.type) {
-      default: return state;
-      // do reducer stuff
-  }
+export default function reducer(state = initialState, action = {}) {
+    switch (action.type) {
+        case "bucket/LOAD": {
+            return state;
+        }
+
+        case "bucket/CREATE" : {
+            const new_bucket_list = [...state.list, action.bucket];
+            return {list: new_bucket_list};
+        }
+
+        default:
+            return state;
+        // do reducer stuff
+    }
 }
 
 
 export function removeWidget(widget) {
-  return { type: REMOVE, widget };
+    return {type: REMOVE, widget};
 }
