1385602ec6d943b35414141653928c5a4d89e688
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-08-09 01:25:58 +0900

    feat : 함수형 컴포넌트에서 리덕스 데이터 사용 - useSelector() 적용

--- bucket_list/src/BucketList.js
+++ bucket_list/src/BucketList.js
@@ -2,13 +2,14 @@
 import React from "react";
 import styled from "styled-components";
 
+import {useSelector, useDispatch} from "react-redux";
+
 const BucketList = (props) => {
-  console.log(props);
-  const my_lists = props.list;
+  const bucket_list = useSelector(state => state.bucket.list);
 
   return (
     <ListStyle>
-      {my_lists.map((list, index) => {
+      {bucket_list.map((list, index) => {
         return (
           <ItemStyle className="list_item" key={index} onClick={() => {
             props.history.push('/details')}}>
