fb1329dfc9a76621e6a12da283fbc5db1306e59b
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-08-10 23:50:55 +0900

    feat: 버튼, 내용 디자인 수정

--- bucket_list/src/Details.js
+++ bucket_list/src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {useSelector, useDispatch} from "react-redux";
 import {deleteBucket, updateBucket} from "./redux/modules/bucket";
+import styled from "styled-components";
 
 const Details = (props) => {
     const dispatch = useDispatch();
@@ -12,17 +13,41 @@ const Details = (props) => {
 
     return (
         <div>
-            <h1>{bucket_list[bucket_index].text}</h1>
-            <button onClick={() => {
-                dispatch(deleteBucket(bucket_index));
-                props.history.goBack();
-            }}>삭제하기</button>
+            <BucketName>🔮 {bucket_list[bucket_index].text} 🔮</BucketName>
 
-            <button onClick={() => {
-                dispatch(updateBucket(bucket_index));
-                props.history.goBack();
-            }}>완료하기</button>
+            <ButtonGroup>
+                <Button onClick={() => {
+                    props.history.goBack();
+                }}> 뒤로가기</Button>
+                <Button onClick={() => {
+                    dispatch(updateBucket(bucket_index));
+                    props.history.goBack();
+                }}>완료하기</Button>
+                <Button onClick={() => {
+                    dispatch(deleteBucket(bucket_index));
+                    props.history.goBack();
+                }}>삭제하기</Button>
+            </ButtonGroup>
         </div>);
 };
 
+const BucketName = styled.h1`
+  color: #F48FB1;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.p`
+    text-align: center;
+`;
+
+const Button = styled.button`
+    width : 90px;
+    background-color: pink;
+    border-radius: 150px;
+    padding: 5px 10px;
+    margin : 10px;
+    border-style: solid;
+    border-color: lightgray;
+    
+`;
 export default Details
