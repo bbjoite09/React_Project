5370870f9019e54f9f0e948a6ccdec3ebe25165a
Heeeon <bbjoite09@naver.com>
2021-08-10 16:46:01 +0900

    feat : move에 opacity, left, background-color 속성 추가

--- keyframes_ex/src/App.js
+++ keyframes_ex/src/App.js
@@ -11,14 +11,30 @@ function App() {
 const move = keyframes`
     0%{
         top : 20px;
+        opacity: 1;
+        left: 20px;
+        background-color: pink;
+    }
+    
+    30%{
+        top: 180px;
+        background-color: orange;
     }
     
     50%{
         top: 200px;
+        opacity: 0;
+        left: 200px;
+    }
+    
+    80%{
+        top:40px;
     }
     
     100%{
         top:20px;
+        opacity: 1;
+        background-color: pink;
     }
 `;
 
@@ -27,10 +43,11 @@ const Box = styled.div`
     height: 150px;
     background-color: green;
     border-radius:150px;
+    opacity: 1;
     position: absolute;
     top: 20px;
     left: 20px;
-    animation: ${move} 2s 1s infinite;
+    animation: ${move} 3s 1s infinite;
 `;
 
 export default App;
