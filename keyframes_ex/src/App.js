4b07cfe683bc4ada7452db5fc57c545947e08625
Heeeon <bbjoite09@naver.com>
2021-08-10 16:23:15 +0900

    feat : ADD animation - 공 튕기기

--- keyframes_ex/src/App.js
+++ keyframes_ex/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import styled, {keyframes} from "styled-components";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box></Box>
     </div>
   );
 }
+const move = keyframes`
+    0%{
+        top : 20px;
+    }
+    
+    50%{
+        top: 200px;
+    }
+    
+    100%{
+        top:20px;
+    }
+`;
+
+const Box = styled.div`
+    width: 150px;
+    height: 150px;
+    background-color: green;
+    border-radius:150px;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    animation: ${move} 2s 1s infinite;
+`;
 
 export default App;
