c1a39008f60e0c6953949d7bf71cf31e96dbe945
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-09-03 03:50:18 +0900

    feat: quiz

--- quiz/src/redux/modules/configStore.js
+++ quiz/src/redux/modules/configStore.js
@@ -1,12 +1,13 @@
-import {createStore, combineReducers} from "redux";
-import quiz from "./quiz"
-import rank from "./rank"
-
-import {createBrowserHistory} from "history";
+import { createStore, combineReducers} from "redux";
+import quiz from "./modules/quiz";
+import rank from "./modules/rank";
+import { createBrowserHistory } from "history";
 
 export const history = createBrowserHistory();
 
-const rootReducer = combineReducers({quiz, rank});
+const middlewares = [thunk];
+
+const rootReducer = combineReducers({ quiz, rank });
 const store = createStore(rootReducer);
 
 export default store;
