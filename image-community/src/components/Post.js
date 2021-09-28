926970b06fb1cf3f2b221c05c365951e5cc87944
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-09-28 23:27:55 +0900

    feat: Text, Image 컴포넌트 생성, 배치

--- image-community/src/components/Post.js
+++ image-community/src/components/Post.js
@@ -1,12 +1,29 @@
 import React from "react";
+// import Grid from "../elements/Grid"
+// import Image from "../elements/Image";
+// import Text from "../elements/Text";
+import {Grid, Image, Text} from "../elements";
 
 const Post = (props) => {
     return (
         <React.Fragment>
-            <div>user profile / user name / insert_dt / is_me (edit btn)</div>
-            <div>contents</div>
-            <div>image</div>
-            <div>comment cnt</div>
+            <Grid>
+                <Grid is_flex>
+                    <Image shape="circle" src={props.src}/>
+                    <Text bold>{props.user_info.user_name}</Text>
+                    <Text>{props.insert_dt}</Text>
+                </Grid>
+                <Grid padding="16px">
+                    <Text>{props.contents}</Text>
+                </Grid>
+                <Grid>
+                    <Image shape={"rectangle"} src={props.src}/>
+                </Grid>
+                <Grid padding="16px">
+                    <Text bold>댓글 {props.comment_cnt}개</Text>
+                </Grid>
+
+            </Grid>
         </React.Fragment>
     )
 }
@@ -14,10 +31,10 @@ const Post = (props) => {
 Post.defaultProps = {
     user_info: {
         user_name: "he3on",
-        user_profile: "https://mean0images.s3.ap-northeast-2.amazonaws.com/4.jpeg",
+        user_profile: "https://s3.ap-northeast-2.amazonaws.com/yeonii.shop/temp_image.jpg",
     },
-    image_url: "https://mean0images.s3.ap-northeast-2.amazonaws.com/4.jpeg",
-    contents: "고양이네요!",
+    image_url: "https://s3.ap-northeast-2.amazonaws.com/yeonii.shop/temp_image.jpg",
+    contents: "귀여워요!",
     comment_cnt: 10,
     insert_dt: "2021-02-27 10:00:00",
 }
---
+++ image-community/src/elements/index.js
@@ -0,0 +1,7 @@
+// 여러 모듈을 한번에 불러올 수 있게 해주는 파일
+
+import Grid from "./Grid";
+import Image from "./Image";
+import Text from "./Text";
+
+export {Grid, Image, Text};
