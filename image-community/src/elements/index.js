e2d8e70b456fc753eb3461b049ce1c085e0b6eb8
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-09-30 02:12:44 +0900

    feat: login input 배치

--- image-community/src/elements/index.js
+++ image-community/src/elements/index.js
@@ -3,5 +3,6 @@
 import Grid from "./Grid";
 import Image from "./Image";
 import Text from "./Text";
+import Input from "./Input";
 
-export {Grid, Image, Text};
+export {Grid, Image, Text, Input};
--- image-community/src/pages/Login.js
+++ image-community/src/pages/Login.js
@@ -1,11 +1,28 @@
 import React from "react";
 import styled from "styled-components";
-import Post from "../components/Post";
+import {Text, Grid, Button, Input} from "../elements"
+
 
 const Login = (props) => {
     return (
         <React.Fragment>
+            <Grid padding="16px">
+                <Text size="32px" bold>로그인</Text>
+                <Grid padding={"16px 0px"}>
+                    <Input label="아이디" placeholder="아이디를 입력해주세요"
+                           _onChange={() => {
+                               console.log("아이디 입력 중")
+                           }}></Input>
+                </Grid>
+                <Grid padding={"16px 0px"}>
+                    <Input label="패스워드" placeholder="패스워드를 입력해주세요"
+                           _onChange={() => {
+                               console.log("패스워드 입력 중")
+                           }}></Input>
+                </Grid>
+            </Grid>
 
+            버튼
         </React.Fragment>
     );
 };
