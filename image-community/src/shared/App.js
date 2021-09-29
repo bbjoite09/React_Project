829e6003a462f50c6a28d549c37c1c9845225863
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-09-30 02:30:43 +0900

    feat: Header 생성, 배치

--- image-community/src/shared/App.js
+++ image-community/src/shared/App.js
@@ -4,9 +4,13 @@ import {BrowserRouter, Route} from "react-router-dom";
 import PostList from "../pages/PostList";
 import Login from "../pages/Login"
 
+import Header from "./Header";
+import {Grid} from "../elements"
+
 function App() {
     return (
         <React.Fragment>
+            <Header/>
             <BrowserRouter>
                 <Route path="/" exact component={PostList}/>
                 <Route path="/login" exact component={Login}/>
