1e321312a86c29ccfea005e40d9c478fe12cb354
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-10-05 02:28:42 +0900

    feat: 리덕스, component 연결

--- image-community/src/shared/App.js
+++ image-community/src/shared/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import React from "react";
 import {BrowserRouter, Route} from "react-router-dom";
+import {ConnectedRouter} from "connected-react-router";
+import {history} from "../redux/configureStore";
+
 import PostList from "../pages/PostList";
 import Login from "../pages/Login"
 import Signup from "../pages/Signup";
@@ -12,11 +15,11 @@ function App() {
     return (
         <React.Fragment>
             <Header/>
-            <BrowserRouter>
+            <ConnectedRouter history={history}>
                 <Route path="/" exact component={PostList}/>
                 <Route path="/login" exact component={Login}/>
                 <Route path="/signup" exact component={Signup}/>
-            </BrowserRouter>
+            </ConnectedRouter>
         </React.Fragment>
     );
 }
