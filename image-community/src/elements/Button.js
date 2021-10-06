6206ff3670f60e3b8befa2cce2aadc2dda1b029d
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-10-06 23:15:48 +0900

    feat: 로그인 시 글쓰기 버튼

--- image-community/src/elements/Button.js
+++ image-community/src/elements/Button.js
@@ -1,8 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
-const Button = (props)=> {
-    const {text, _onClick} = props;
+const Button = (props) => {
+    const {text, _onClick, is_float} = props;
+
+    if (is_float) {
+        return (
+            <React.Fragment>
+                <FloatButton onClick={_onClick}>{text}</FloatButton>
+            </React.Fragment>
+        )
+    }
+
     return (
         <React.Fragment>
             <ElButton onClick={_onClick}>{text}</ElButton>
@@ -12,7 +21,8 @@ const Button = (props)=> {
 
 Button.defaultPropos = {
     text: "텍스트",
-    _onClick: ()=> {}
+    _onClick: () => {},
+    is_float: false,
 }
 
 const ElButton = styled.button`
@@ -22,6 +32,22 @@ const ElButton = styled.button`
     padding: 12px 0px;
     box-sizing: border-box;
     border: none;
+`;
+
+const FloatButton = styled.button`
+    width: 50px;
+    height: 50px;
+    background-color: #212121;
+    color: #ffffff;
+    box-sizing: border-box;
+    font-size: 30px;
+    font-weight: 800;
+    position: fixed;
+    bottom: 50px;
+    right: 16px;
+    vertical-align: middle;
+    border: none;
+    border-radius: 50px;
 `;
 
 export default Button;
